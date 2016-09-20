471deb5a4449d473fe53026b1f9acffe65b6ccf2
432ats <ats.8060@gmail.com>
2016-09-20 17:33:31 +0900

    ILookAheadの追加

---
+++ MakeOthello/MakeOthello/Model/FinalMakeLookAhead.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeOthello.Model
+{
+    class FinalMakeLookAhead : ILookAhead
+    {
+        public Point LookAhead(IOthello Othello, int turn)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MakeOthello/MakeOthello/Model/ILookAhead.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeOthello.Model
+{
+    interface ILookAhead
+    {
+        Point LookAhead(IOthello Othello, int turn);
+        //MakeLookAhead,FinalMakeLookAheadを継承
+    }
+}
